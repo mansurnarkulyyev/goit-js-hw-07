4a92682f410569b5bce0f36c0a022a7216522013
Mansur Narkulyyev <50501517+mansurnarkulyyev@users.noreply.github.com>
2022-12-20 17:20:53 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,13 +1,8 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
 console.log(galleryItems);
-
 const listEl = document.querySelector(".gallery");
-// listEl.addEventListener("click", closeModal)
 
 function renderGalleryList(array) {
-
     const listMarkup = array
         .map(({ preview, original, description } = {}) => {
             return `<li><a class="gallery__item" href="${original}">
@@ -17,7 +12,6 @@ function renderGalleryList(array) {
         .join("");
     listEl.insertAdjacentHTML("beforeend", listMarkup);
     console.log(listMarkup);
-
 }
 
 renderGalleryList(galleryItems);
@@ -27,19 +21,3 @@ new SimpleLightbox('.gallery a', {
     captionDelay: 250,
 });
 
-
-
-// function closeModal(event) {
-//     event.preventDefault();
-// new SimpleLightbox('.gallery a', {
-//     captionsData: 'alt',
-//     captionDelay: 250,
-// });
-
-    // console.log(event.target);
-// }
-
-// let lightbox = new SimpleLightbox('.gallery a', {
-//   captionsData: 'alt',
-//   captionDelay: 250,
-// });
