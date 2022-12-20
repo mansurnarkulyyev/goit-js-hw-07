28189b4583352da6e6a733e9a6deb25c545c3498
Mansur Narkulyyev <50501517+mansurnarkulyyev@users.noreply.github.com>
2022-12-20 17:19:54 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,7 +1,5 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-// console.log(galleryItems);
 const listEl = document.querySelector(".gallery");
 
 function renderGalleryList(array) {
@@ -9,106 +7,30 @@ function renderGalleryList(array) {
   const listMarkup = array
     .map(({ preview, original, description } = {}) => {
       return `<li class="gallery__item">
-      <a
-        class="gallery__link"
-        href="${original}"
-        
-      >
+      <a class="gallery__link"
+        href="${original}"  >
         <img
           class="gallery__image"
           src="${preview}"
           data-source="${original}"
-          alt="${description}"
-        />
+          alt="${description}"/>
       </a>
     </li>`;
     })
     .join("");
   listEl.insertAdjacentHTML("beforeend", listMarkup);
   console.log(listMarkup);
-
 }
 
 renderGalleryList(galleryItems);
 
-// listEl.addEventListener('click', onImgContainerClick)
-
 document.querySelector('.gallery').onclick = (event) => {
   event.preventDefault();
-  // if (!event.target.classList.contains('gallery__item')) {
-  //   return;
-  // }
+  
   if (event.target.nodeName !== 'IMG') {
     return;
   }
   basicLightbox.create(`<img src="${event.target.dataset.source}">`).show();
 
-
 }
 
-
-
-
-
-
-
-
-
-
-// const listEl = document.querySelector(".gallery");
-// const modalEl = document.querySelector(".js-lightbox");
-// const modalImgEl = document.querySelector(".lightbox__image");
-// const buttonEl = document.querySelector(".lightbox__button");
-// const ligthBoxEl = document.querySelector(".lightbox__overlay");
-
-// function renderGallaryList(array) {
-//   const listMarkup = array
-//     .map(({ preview, original, description } = {}) => {
-//       return `<li class="gallery__item">
-//     <a
-//       class="gallery__link"
-//       href="${original}"
-//     >
-//       <img
-//         class="gallery__image"
-//         src="${preview}"
-//         data-source="${original}"
-//         alt="${description}"
-//       />
-//     </a>
-//   </li>`;
-//     })
-//     .join("");
-//   listEl.insertAdjacentHTML("beforeend", listMarkup);
-// }
-// renderGallaryList(galleryItems);
-
-// listEl.addEventListener("click", openModal);
-
-// function openModal(event) {
-//   event.preventDefault();
-//   const currentImg = event.target;
-//   if (currentImg.nodeName !== "IMG") {
-//     return;
-//   }
-  //   const urlOrigin = currentImg.dataset.source;
-  //   modalImgEl.src = urlOrigin;
-  //   modalEl.classList.add("is-open");
-
-  //   buttonEl.addEventListener("click", closeModal);
-  //   ligthBoxEl.addEventListener("click", closeModal);
-  //   window.addEventListener("keydown", closeModalEsc);
-// }
-
-// function closeModal() {
-//   modalEl.classList.remove("is-open");
-//   modalImgEl.src = "";
-//   window.removeEventListener("keydown", closeModalEsc);
-// }
-
-// function closeModalEsc(event) {
-//   if (event.code !== "Escape") {
-//     return;
-//   }
-//   closeModal();
-// }
